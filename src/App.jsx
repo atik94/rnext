388bba92b534ca4ece33c886759cf1148dc29657
atik94 <atikurrahmanatik94@gmail.com>
2024-02-26 15:43:07 +0600

    cinerental project is complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
+import { MovieContext, ThemeContext } from "./context";
+import Page from "./Page";
+
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
+  const [cartData, setCartData] = useState([]);
   return (
     <>
-      <p>Welcome To Tasker</p>
+      <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
+        <MovieContext.Provider value={{ cartData, setCartData }}>
+          <Page />
+        </MovieContext.Provider>
+      </ThemeContext.Provider>
     </>
   );
 }
